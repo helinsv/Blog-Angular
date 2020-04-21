8f4171814adebb2fc8afbdb0cae97b3e70531960
helinsv <helinsv@gmail.com>
2020-04-21 12:22:44 +0100

    Added structure and routing

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, ROUTER_INITIALIZER } from '@angular/router';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from '../admin/dashboard-page/dashboard-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+
+@NgModule({
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: AdminLayoutComponent, children: [
+          { path: '', pathMatch: 'full', redirectTo: '/admin/login' },
+          { path: 'login', component: LoginPageComponent },
+          { path: 'dashboard', component: DashboardPageComponent },
+          { path: 'post/:id/edit', component: EditPageComponent },
+          { path: 'create', component: CreatePageComponent }
+        ]
+      }
+    ])
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AdminModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {PostPageComponent} from './post-page/post-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      { path: '', pathMatch: 'full', redirectTo: '/' },
+      { path: '', component: HomePageComponent },
+      { path: 'post/:id', component: PostPageComponent }
+    ],
+    // { path: '**', component: PathNotFoundComponent }
+  },
+  //{ path: 'admin', loadChildren: '../app/admin/admin.module#AdminModule' }
+  { path: 'admin', loadChildren: () => import('../app/admin/admin.module').then(m => m.AdminModule) },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
