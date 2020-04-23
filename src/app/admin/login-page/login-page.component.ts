01fc4de1dca3cdcfc86ee8ccdadf66e41ac08eee
helinsv <helinsv@gmail.com>
2020-04-23 10:10:13 +0100

    added login form

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { User } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
@@ -8,12 +9,13 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 })
 export class LoginPageComponent implements OnInit {
   form: FormGroup;
+  //user: User[];
   constructor() { }
 
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl('', [Validators.minLength(6), Validators.required]),
+      password: new FormControl('', [Validators.minLength(3), Validators.required]),
     })
   }
 
@@ -21,6 +23,16 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+
+    console.log(user);
+    this.form.reset();
+
+
   }
 
 
