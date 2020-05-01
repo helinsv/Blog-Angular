74d76ccec30107f0df52d8646071c843341b934a
Helin <helinsc@gmail.com>
2020-05-01 13:30:12 +0100

    Added new post form with validation and text editor

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Post } from '../../shared/interfaces'
 @Component({
   selector: 'app-create-page',
   templateUrl: './create-page.component.html',
@@ -9,12 +10,31 @@ export class CreatePageComponent implements OnInit {
 
   constructor() { }
 
+  form: FormGroup;
 
   ngOnInit() {
 
-
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      author: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      text: new FormControl('', [Validators.required, Validators.minLength(3)])
+    })
   }
 
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
 
+    const post: Post = {
+      title: this.form.value.title,
+      author: this.form.value.author,
+      text: this.form.value.text,
+      date: new Date()
+    };
+
+    console.log(post);
+    this.form.reset();
+  }
 
 }
